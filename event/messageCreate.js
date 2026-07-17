87ba8b1af44893ab95f31d18babe20a950b4ac1d
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-17 14:20:38 -0400

    Update and rename events.js to messageCreate.js

---
+++ event/messageCreate.js
@@ -0,0 +1,17 @@
+const PREFIX = '!';
+
+module.exports = {
+  name: 'messageCreate',
+  execute(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(PREFIX)) return;
+
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = message.client.commands.get(commandName);
+    if (!command) return;
+
+    command.execute(message, args);
+  },
+};
