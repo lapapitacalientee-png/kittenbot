d6cabd7b0fc9cd2a8d8fac8893a67aff9c6fb4b0
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-17 14:25:55 -0400

    Update and rename utils to gamenights.js

---
+++ utils/gamenights.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, '..', 'data', 'gamenights.json');
+
+function loadData() {
+  if (!fs.existsSync(dataPath)) {
+    fs.mkdirSync(path.dirname(dataPath), { recursive: true });
+    fs.writeFileSync(dataPath, '{}');
+  }
+  return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+}
+
+function saveData(data) {
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+}
+
+function addGamenights(userId, amount) {
+  const data = loadData();
+  data[userId] = (data[userId] || 0) + amount;
+  saveData(data);
+  return data[userId];
+}
+
+module.exports = { loadData, saveData, addGamenights };
