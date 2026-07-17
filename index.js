578b0e6d0ffd44d5d7f8f3e5c6f5874ec25cec80
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-17 14:03:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new Client({
 
 // Cargar comandos
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, 'command');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -23,7 +23,7 @@ for (const file of commandFiles) {
 }
 
 // Cargar eventos
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, 'event');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
